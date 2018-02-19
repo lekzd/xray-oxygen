eb86b1be732bc9c0b0a648441d5344469678a40c
Giperion <dereline@hotmail.com>
2018-02-19 21:53:07 +0300

    * FIX: D3D12 includes * ADD: Microsoft::WRL namespace classes * NEW: D3D12 check function

--- code/engine.vc2008/xrRender/xrRenderPC_R5/r2_test_hw.cpp
+++ code/engine.vc2008/xrRender/xrRenderPC_R5/r2_test_hw.cpp
@@ -20,19 +20,14 @@ typedef HRESULT (__stdcall *FuncPtrD3D11CreateDeviceAndSwapChain)(
     ID3D11DeviceContext** ppImmediateContext );
 #endif
 #ifdef USE_DX12
-typedef HRESULT(__stdcall *FuncPtrD3D12CreateDeviceAndSwapChain)(
-	IDXGIAdapter* pAdapter,
-	D3D_DRIVER_TYPE DriverType,
-	HMODULE Software,
-	UINT Flags,
-	CONST D3D_FEATURE_LEVEL* pFeatureLevels,
-	UINT FeatureLevels,
-	UINT SDKVersion,
-	CONST DXGI_SWAP_CHAIN_DESC* pSwapChainDesc,
-	IDXGISwapChain** ppSwapChain,
-	ID3D12Device** ppDevice,
-	D3D_FEATURE_LEVEL* pFeatureLevel,
-	ID3DDeviceContext** ppImmediateContext);
+
+typedef HRESULT (__stdcall *FuncPtrD3D12CreateDevice)(
+    IUnknown          *pAdapter,
+    D3D_FEATURE_LEVEL MinimumFeatureLevel,
+    REFIID            riid,
+    void              **ppDevice
+);
+
 #endif
 #ifdef USE_DX11
 bool TestDX11Present()
@@ -41,12 +36,12 @@ bool TestDX11Present()
 
 	if (!hD3D11)
 	{
-		Msg("* DX11: failed to load d3d12.dll");
+		Msg("* DX11: failed to load d3d11.dll");
 		return false;
 	}
 	
 	FuncPtrD3D11CreateDeviceAndSwapChain pD3D11CreateDeviceAndSwapChain = 
-		(FuncPtrD3D11CreateDeviceAndSwapChain)GetProcAddress(hD3D11, "D3D12CreateDeviceAndSwapChain");
+		(FuncPtrD3D11CreateDeviceAndSwapChain)GetProcAddress(hD3D11, "D3D11CreateDeviceAndSwapChain");
 
 	if (!pD3D11CreateDeviceAndSwapChain)
 	{
@@ -121,6 +116,7 @@ bool TestDX11Present()
 	return SUCCEEDED(hr);
 }
 #endif
+
 #ifdef USE_DX12
 bool TestDX12Present()
 {
@@ -132,12 +128,12 @@ bool TestDX12Present()
 		return false;
 	}
 
-	FuncPtrD3D12CreateDeviceAndSwapChain pD3D12CreateDeviceAndSwapChain =
-		(FuncPtrD3D12CreateDeviceAndSwapChain)GetProcAddress(hD3D12, "D3D12CreateDeviceAndSwapChain");
+    FuncPtrD3D12CreateDevice pD3D12CreateDevice =
+		(FuncPtrD3D12CreateDevice)GetProcAddress(hD3D12, "D3D12CreateDevice");
 
-	if (!pD3D12CreateDeviceAndSwapChain)
+	if (!pD3D12CreateDevice)
 	{
-		Msg("* DX12: failed to get address of D3D12CreateDeviceAndSwapChain");
+		Msg("* DX12: failed to get address of D3D12CreateDevice");
 		return false;
 	}
 
@@ -161,7 +157,6 @@ bool TestDX12Present()
 		CW_USEDEFAULT, CW_USEDEFAULT,
 		NULL, NULL, NULL, NULL);
 
-	DXGI_SWAP_CHAIN_DESC sd;
 
 	if (!hWnd)
 	{
@@ -169,43 +164,52 @@ bool TestDX12Present()
 		return false;
 	}
 
-	HRESULT hr = E_FAIL;
+    ComPtr<IDXGIFactory4> DXGIFactory;
+    if (!SUCCEEDED(CreateDXGIFactory2(0, IID_PPV_ARGS(&DXGIFactory))))
+    {
+        FreeLibrary(hD3D12);
 
-	std::memset(&sd, 0, sizeof(sd));
-	sd.BufferCount = 1;
-	sd.BufferDesc.Width = 800;
-	sd.BufferDesc.Height = 600;
-	sd.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
-	sd.BufferDesc.RefreshRate.Numerator = 60;
-	sd.BufferDesc.RefreshRate.Denominator = 1;
-	sd.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
-	sd.OutputWindow = hWnd;
-	sd.SampleDesc.Count = 1;
-	sd.SampleDesc.Quality = 0;
-	sd.Windowed = TRUE;
+        DestroyWindow(hWnd);
+        return false;
+    }
 
-	D3D_FEATURE_LEVEL pFeatureLevels[] = { D3D_FEATURE_LEVEL_12_0 };
-	D3D_FEATURE_LEVEL FeatureLevel;
+    ComPtr<IDXGIAdapter1> HardwareAdapter;
 
-	ID3D12Device*           pd3dDevice			= NULL;
-	ID3D12Device*			pContext			= NULL;
-	IDXGISwapChain*         pSwapChain			= NULL;
+    for (UINT adapterIndex = 0; DXGI_ERROR_NOT_FOUND != DXGIFactory->EnumAdapters1(adapterIndex, &HardwareAdapter); ++adapterIndex)
+    {
+        DXGI_ADAPTER_DESC1 desc;
+        HardwareAdapter->GetDesc1(&desc);
 
-	/*hr = pD3D12CreateDeviceAndSwapChain(NULL, D3D_DRIVER_TYPE_HARDWARE, NULL, 0, pFeatureLevels, 1,
-		D3D12_SDK_VERSION, &sd, &pSwapChain, &pd3dDevice, &FeatureLevel, &pContext);
+        if (desc.Flags & DXGI_ADAPTER_FLAG_SOFTWARE)
+        {
+            // Don't select the Basic Render Driver adapter.
+            // If you want a software adapter, pass in "/warp" on the command line.
+            continue;
+        }
 
-	if (FAILED(hr))
-		Msg("* D3D12: device creation failed with hr=0x%08x", hr);
-		*/
-	if (pContext) pContext->Release();
-	if (pSwapChain) pSwapChain->Release();
-	if (pd3dDevice) pd3dDevice->Release();
+        // Check to see if the adapter supports Direct3D 12, but don't create the
+        // actual device yet.
+        if (SUCCEEDED(D3D12CreateDevice(HardwareAdapter.Get(), D3D_FEATURE_LEVEL_11_0, _uuidof(ID3D12Device), nullptr)))
+        {
+            break;
+        }
+    }
+
+    if (HardwareAdapter.Get() == nullptr)
+    {
+        FreeLibrary(hD3D12);
+
+        DestroyWindow(hWnd);
+        return false;
+    }
+
+
 
 	FreeLibrary(hD3D12);
 
 	DestroyWindow(hWnd);
 
-	return SUCCEEDED(hr);
+	return true;
 }
 #endif
 BOOL	xrRender_test_hw		()
